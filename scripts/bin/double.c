a500885ba71d4911aa1e70b6881c6f853f7801bf
Matteo Bonora <bonora.matteo@gmail.com>
2017-11-13 10:03:32 +0100

    Bad boi

---
+++ scripts/bin/double.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+# define double x = x +
+
+int main(int argc, char **argv)
+{
+  int x;
+  if(argc > 1)
+    x = atoi(argv[1]);
+  
+  double x;
+  printf("%d\n",x);
+  return 0;
+}

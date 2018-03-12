e7342435731a167fb652d0fff9bca6d9b5b916d0
Matteo Bonora <bonora.matteo@gmail.com>
2018-03-12 15:02:48 +0100

    Update

--- scripts/bin/backup.sh
+++ scripts/bin/backup.sh
@@ -4,8 +4,16 @@ path=$(mktemp -d)
 disk=$1
 backpath=$path/$HOSTNAME/$HOSTNAME-$(date +%Y-%m-%d).img.gz
 
+corever=`dd --version | head -n1 | awk '{print $3}'`
+minver=8.24
+
+ddcmd='dd if=$disk'
+[ $(sort -V <(echo $corever) <(echo $minver) | head -n1) == "$minver" ] && ddcmd="$ddcmd status=progress"
+
+echo $ddcmd
+
 if mount 192.168.1.14:/c/dimages $path ; then
-  dd if=$disk status=progress | pigz -c > $backpath
+  $ddcmd | pigz -c > $backpath
   chown bonnee:bonnee $backpath
 
   [ $(ls -1 | wc -l) -gt 3 ] && rm $(ls -1t | tail -1)

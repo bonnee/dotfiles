d747ba08233d812032aea8392134e73e33a384e9
Matteo Bonora <bonnee@Argo>
2018-03-12 14:26:46 +0100

    Update backup.sh

--- scripts/bin/backup.sh
+++ scripts/bin/backup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 path=$(mktemp -d)
+disk=$1
 backpath=$path/$HOSTNAME/$HOSTNAME-$(date +%Y-%m-%d).img.gz
 
-
 if mount 192.168.1.14:/c/dimages $path ; then
-  dd if=/dev/mmcblk0 status=progress | pigz -c > $backpath
+  dd if=$disk status=progress | pigz -c > $backpath
   chown bonnee:bonnee $backpath
 
   [ $(ls -1 | wc -l) -gt 3 ] && rm $(ls -1t | tail -1)

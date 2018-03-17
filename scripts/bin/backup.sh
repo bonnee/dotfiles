4dc214622571d039bcda424702d2a8c0b94f7485
Bonnee <bonora.matteo@gmail.com>
2018-03-17 10:22:22 +0100

    Fix and update backup.sh

--- scripts/bin/backup.sh
+++ scripts/bin/backup.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/env bash
 
 disk=$1
+backupmount=192.168.1.14:/c/dimages 
 
 corever=`dd --version | head -n1 | awk '{print $3}'`
 minver=8.24
 
 ddcmd="dd if=$disk"
-[ $(sort -V <(echo $corever) <(echo $minver) | head -n1) == "$minver" ] && ddcmd="$ddcmd status=progress"
+[ $(sort -V <(echo $corever) <(echo $minver) | head -n1) == "$minver" ] && ddcmd="$ddcmd status=progress" && echo "Showing progress"
 
-if command -v pigz; then gzipcmd="pigz -c"; else gzipcmd="gzip -c"; fi
+if command -v pigz > /dev/null; then gzipcmd="pigz"; else gzipcmd="gzip"; fi
 
-echo $gzipcmd
-echo $ddcmd
+echo "Using $gzipcmd"
 
-if mount 192.168.1.14:/c/dimages $path ; then
-  path=$(mktemp -d)
+path=$(mktemp -d)
+printf "Mounting..."
+if mount $backupmount $path ; then
+  printf "Backupping...\n"
+
   backpath=$path/$HOSTNAME/$HOSTNAME-$(date +%Y-%m-%d).img.gz
+  ! $ddcmd | $gzipcmd -c > $backpath  && rm $backpath
 
-  $ddcmd | $gzipcmd > $backpath
-  chown bonnee:bonnee $backpath
+  old=`ls -1t $path/$HOSTNAME | tail -1`
+  [ $(ls -1 $path/$HOSTNAME | wc -l) -gt 3 ] && echo "Removing $old" && rm $path/$HOSTNAME/$old
 
-  [ $(ls -1 | wc -l) -gt 3 ] && rm $(ls -1t | tail -1)
-
+  echo "Unmounting and cleanup..."
   umount $path && rm -rf $path
 fi
 

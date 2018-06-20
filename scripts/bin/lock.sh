570598fbe280fc1e3a73b74f568c17b46074fe87
Bonnee <bonora.matteo@gmail.com>
2018-06-20 17:30:50 +0200

    Unify colors

--- scripts/bin/lock.sh
+++ scripts/bin/lock.sh
@@ -2,9 +2,39 @@
 
 tmpbg='/tmp/screen.png'
 
-(( $# )) && { icon=$1; }
+trans="#00000000"
+black="$(xgetres color8)BB"
+white="$(xgetres color15)FF"
+blue="$(xgetres color4)FF"
+red="$(xgetres color1)FF"
+green="$(xgetres color10)FF"
 
 scrot "$tmpbg"
-convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
-i3lock -i "$tmpbg"
-#sleep 1
+convert "$tmpbg" -scale 50% -scale 10% -scale 1000% -scale 200% "$tmpbg"
+i3lock --clock --indicator \
+\
+--insidevercolor=$black \
+--ringvercolor=$blue \
+\
+--insidewrongcolor=$black \
+--ringwrongcolor=$red \
+\
+--insidecolor=$black \
+--ringcolor=$trans \
+--line-uses-ring \
+--separatorcolor=$trans \
+--keyhlcolor=$green \
+--bshlcolor=$red \
+\
+--verifcolor=$white \
+--wrongcolor=$white \
+--timecolor=$white \
+--datecolor=$white \
+--layoutcolor=$white \
+\
+--veriftext="Mhm..." \
+--wrongtext="Nope!" \
+--timestr="%H:%M" \
+--datestr="%a %d %b" \
+\
+-i "$tmpbg" 

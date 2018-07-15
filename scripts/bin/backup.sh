fdd77c793d50e512408e411c3b40feec8f50e22d
Matteo Bonora <bonora.matteo@gmail.com>
2018-07-16 00:40:44 +0200

    Update backup

--- scripts/bin/backup.sh
+++ scripts/bin/backup.sh
@@ -3,11 +3,13 @@
 disk=$1
 backupmount=192.168.1.14:/c/dimages
 
-corever=`dd --version | head -n1 | awk '{print $3}'`
-minver=8.24
-
 ddcmd="dd if=$disk"
-[ $(sort -V <(echo $corever) <(echo $minver) | head -n1) == "$minver" ] && ddcmd="$ddcmd status=progress" && printf "Showing progress. "
+
+if command -v pv > /dev/null; then
+  disksize=$(fdisk -l $1 | grep Disk | head -n1 | awk '{print $5}')
+  statcmd="pv -s $disksize"
+  echo "Using pv"
+fi
 
 if command -v pigz > /dev/null; then gzipcmd="pigz"; else gzipcmd="gzip"; fi
 
@@ -19,7 +21,7 @@ if mount "$backupmount" "$path" ; then
   printf "Backupping...\n"
 
   backpath="$path/$HOSTNAME/$HOSTNAME-$(date +%Y-%m-%d).img.gz"
-  ! $ddcmd | $gzipcmd -c > "$backpath"  && echo "Backup failed. Removing file" && rm "$backpath"
+  ! $ddcmd | $statcmd | $gzipcmd -c > "$backpath"  && echo "Backup failed. Removing file" && rm "$backpath"
 
   old=`ls -1t "$path/$HOSTNAME" | tail -1`
   [ $(ls -1 "$path/$HOSTNAME" | wc -l) -gt 3 ] && echo "Removing $old" && rm "$path/$HOSTNAME/$old"

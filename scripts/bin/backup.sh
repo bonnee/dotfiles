5b879096fef43064ffc4bb7c92f2130369be41a5
Bonnee <bonora.matteo@gmail.com>
2018-07-17 17:07:54 +0200

    Refactor backup.sh

--- scripts/bin/backup.sh
+++ scripts/bin/backup.sh
@@ -1,32 +1,80 @@
 #!/usr/bin/env bash
 
 disk=$1
-backupmount=192.168.1.14:/c/dimages
+gzip=1
+remote="192.168.1.14:/c/dimages"
+mount_point=$(mktemp -d)
+subdir="$HOSTNAME"
+dir="$mount_point/$subdir"
+
+backup_image="$HOSTNAME-$(date +%Y-%m-%d).img"
+log_name="$HOSTNAME.log"
 
 ddcmd="dd if=$disk"
 
+function log {
+  echo "$(date --rfc-3339=seconds)" "$1" >> "$dir/$log_name"
+}
+
+function msg {
+  echo "$1"
+  log "$1"
+}
+
+printf "Mounting $remote..."
+if mount "$remote" "$mount_point" ; then
+  echo "Done."
+else
+  echo "Error mounting."
+  exit 1
+fi
+
+sync; sync
+
+log "New backup starting"
+
 if command -v pv > /dev/null; then
-  disksize=$(fdisk -l $1 | grep Disk | head -n1 | awk '{print $5}')
+  disksize=$(blockdev --getsize64 $disk)
   statcmd="pv -s $disksize"
-  echo "Using pv"
+  log "Using pv"
 fi
 
-if command -v pigz > /dev/null; then gzipcmd="pigz"; else gzipcmd="gzip"; fi
+if [ $gzip = 1 ]; then
 
-echo "Using $gzipcmd"
+  if command -v pigz > /dev/null; then
+    gzipcmd="pigz -c";
+  else
+    gzipcmd="gzip -c"
+  fi
+  log "Compression enabled with '$gzipcmd'"
 
-path=$(mktemp -d)
-printf "Mounting..."
-if mount "$backupmount" "$path" ; then
-  printf "Backupping...\n"
+  backup_image="$backup_image.gz"
 
-  backpath="$path/$HOSTNAME/$HOSTNAME-$(date +%Y-%m-%d).img.gz"
-  ! $ddcmd | $statcmd | $gzipcmd -c > "$backpath"  && echo "Backup failed. Removing file" && rm "$backpath"
+  msg "Backup started"
+  log "Saving in '$backup_image'"
+  $ddcmd | $statcmd | $gzipcmd > "$dir/$backup_image"
+else
+  msg "Compression disabled"
 
-  old=`ls -1t "$path/$HOSTNAME" | tail -1`
-  [ $(ls -1 "$path/$HOSTNAME" | wc -l) -gt 3 ] && echo "Removing $old" && rm "$path/$HOSTNAME/$old"
-
-  echo "Unmounting and cleanup..."
-  umount "$path" && rm -rf "$path"
+  msg "Backup started"
+  log "Saving in '$backup_image'"
+  $ddcmd | $statcmd > "$dir/$backup_image"
 fi
+
+state=$?
+
+if [ $state = 0 ]; then
+  msg "Backup succedded."
+  old=`ls -1t "$mount_point/$subdir" | tail -1`
+  [ $(ls -1 "$mount_point/$subdir" | wc -l) -gt 3 ] && msg "Removing $old" && rm "$dir/$old" && msg "$old removed."
+else
+  msg "Backup failed. Cleaning up"
+  rm "$dir/$backup_image"
+  echo "File system cleaned."
+  log "'$backup_image' removed."
+fi
+
+log "Bye."
+printf "Unmounting..."
+umount "$mount_point" && rm -rf "$mount_point" && echo "Done."
 

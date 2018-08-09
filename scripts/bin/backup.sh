6b934914f4d7c790ab98d1ac2523b9051cd701bc
Bonnee <bonora.matteo@gmail.com>
2018-08-09 22:07:23 +0200

    Fix backup.sh

--- scripts/bin/backup.sh
+++ scripts/bin/backup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 disk=$1
+headless=${2:-0}
 gzip=1
 remote="192.168.1.14:/c/dimages"
 mount_point=$(mktemp -d)
@@ -12,13 +13,22 @@ log_name="$HOSTNAME.log"
 
 ddcmd="dd if=$disk"
 
-function log {
+log() {
   echo "$(date --rfc-3339=seconds)" "$1" >> "$dir/$log_name"
 }
 
-function msg {
+msg() {
   echo "$1"
   log "$1"
+}
+
+count()
+{
+  echo "$1" | wc -l
+}
+
+first() {
+  echo "$1" | head -1
 }
 
 printf "Mounting $remote..."
@@ -29,19 +39,21 @@ else
   exit 1
 fi
 
+if ! [ "$headless" = 1 ] && ! command -v pv > /dev/null ; then
+  msg "pv not found. Using headless mode"
+  headless=1
+fi
+
 sync; sync
 
 log "New backup starting"
 
-if command -v pv > /dev/null; then
-  disksize=$(blockdev --getsize64 $disk)
-  statcmd="pv -s $disksize"
-  log "Using pv"
-fi
+disksize=$(blockdev --getsize64 $disk)
+statcmd="pv -s $disksize"
 
-if [ $gzip = 1 ]; then
+if [ "$gzip" = 1 ]; then
 
-  if command -v pigz > /dev/null; then
+  if command -v pigz > /dev/null ; then
     gzipcmd="pigz -c";
   else
     gzipcmd="gzip -c"
@@ -50,23 +62,42 @@ if [ $gzip = 1 ]; then
 
   backup_image="$backup_image.gz"
 
-  msg "Backup started"
-  log "Saving in '$backup_image'"
-  $ddcmd | $statcmd | $gzipcmd > "$dir/$backup_image"
+  msg "Backup starting"
+  if [ "$headless" = 1 ]; then
+    log "Executing '$ddcmd | $gzipcmd > \"$dir/$backup_image\"'"
+    $ddcmd | $gzipcmd > "$dir/$backup_image"
+  else
+    log "Executing '$ddcmd | $statcmd | $gzipcmd > \"$dir/$backup_image\"'"
+    $ddcmd | $statcmd | $gzipcmd > "$dir/$backup_image"
+  fi
 else
   msg "Compression disabled"
 
-  msg "Backup started"
-  log "Saving in '$backup_image'"
-  $ddcmd | $statcmd > "$dir/$backup_image"
+  msg "Backup starting"
+  if [ "$headless" = 1 ]; then
+    log "Executing '$ddcmd > \"$dir/$backup_image\"'"
+    $ddcmd > "$dir/$backup_image"
+  else
+    log "Executing '$ddcmd | $statcmd > \"$dir/$backup_image\"'"
+    $ddcmd | $statcmd > "$dir/$backup_image"
+  fi
 fi
 
 state=$?
 
-if [ $state = 0 ]; then
+if [ "$state" = 0 ]; then
   msg "Backup succedded."
-  old=`ls -1t "$mount_point/$subdir" | tail -1`
-  [ $(ls -1 "$mount_point/$subdir" | wc -l) -gt 3 ] && msg "Removing $old" && rm "$dir/$old" && msg "$old removed."
+
+  ifs=$IFS
+  IFS= # https://unix.stackexchange.com/a/164548
+  imgs=$(ls -1tr "$dir" | grep -E "(img.gz$|img$)")
+
+  if [ $(count "$imgs") -gt 3 ] ; then
+    old=$(first $imgs)
+    msg "Removing $old" && rm "$dir/$old" && msg "$old removed."
+
+  fi
+  IFS=$ifs
 else
   msg "Backup failed. Cleaning up"
   rm "$dir/$backup_image"
@@ -77,4 +108,3 @@ fi
 log "Bye."
 printf "Unmounting..."
 umount "$mount_point" && rm -rf "$mount_point" && echo "Done."
-

02f088cb721648f648e9c936328790a93637b1ce
Bonnee <bonora.matteo@gmail.com>
2020-05-04 17:04:12 +0200

    Theme swaylock

--- bin/lockw.sh
+++ bin/lockw.sh
@@ -1,36 +1,39 @@
 #!/usr/bin/env bash
 
 [ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -en "$0" "$0" "$@" || :
+. "$HOME/.cache/wal/colors.sh"
 
 tmpbg='/tmp/screen.png'
 
 trans="#00000000"
-black="#000000BB"
-white="#F6F6F6FF"
-blue="#2196F3FF"
-red="#F44336FF"
-green="#4CAF50FF"
+alpha="D9"
 
 grim "$tmpbg"
 convert "$tmpbg" -scale 50% -scale 10% -scale 1000% -scale 200% "$tmpbg"
 
 swaylock -f \
+--ignore-empty-password \
 \
 --indicator-radius=150 \
---inside-ver-color="$black" \
---ring-ver-color="$blue" \
+--inside-ver-color="$background$alpha" \
+--ring-ver-color="$color2" \
 \
---inside-wrong-color="$black" \
---ring-wrong-color="$red" \
+--text-clear-color="$background" \
+--inside-clear-color="$foreground" \
+--line-clear-color="$foreground" \
+--ring-clear-color="$foreground" \
 \
---inside-color="$black" \
+--inside-wrong-color="$background$alpha" \
+--ring-wrong-color="$color6" \
+\
+--inside-color="$background$alpha" \
 --ring-color="$trans" \
 --line-uses-ring \
 --separator-color="$trans" \
---key-hl-color="$green" \
---bs-hl-color="$red" \
+--key-hl-color="$color1" \
+--bs-hl-color="$color6" \
 \
---text-ver-color="$white" \
---text-wrong-color="$white" \
+--text-ver-color="$foreground" \
+--text-wrong-color="$foreground" \
 \
 -i "$tmpbg"

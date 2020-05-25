ae6b4a22ab07411a4bbdca28e2fa43a0b411ed57
Bonnee <bonora.matteo@gmail.com>
2020-05-25 12:10:42 +0200

    Add dotfiles install script

---
+++ bin/dot-install.sh
@@ -0,0 +1,16 @@
+dotdir=$HOME/.dotfiles
+
+git clone --bare https://github.com/Bonnee/dotfiles.git $dotdir
+function config {
+   /usr/bin/git --git-dir=$dotdir --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no

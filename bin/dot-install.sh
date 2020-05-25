16d0bd5caa331eee00f13a0d2f783f805b749d4e
Matteo Bonora <bonora.matteo@gmail.com>
2020-05-25 12:15:17 +0200

    Update dot-install.sh

--- bin/dot-install.sh
+++ bin/dot-install.sh
@@ -1,4 +1,5 @@
 dotdir=$HOME/.dotfiles
+alias dots='/usr/bin/git --git-dir=$dotdir --work-tree=$HOME'
 
 git clone --bare https://github.com/Bonnee/dotfiles.git $dotdir
 function config {
@@ -10,7 +11,7 @@ if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    dots checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dots-backup/{}
 fi;
 config checkout
 config config status.showUntrackedFiles no

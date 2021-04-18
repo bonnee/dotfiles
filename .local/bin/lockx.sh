1d05acf5eac7835dd0f485dbbdf191d260a376f6
Bonnee <bonora.matteo@gmail.com>
2021-04-19 01:24:00 +0200

    lockx: turn off table lamp when locking

--- .local/bin/lockx.sh
+++ .local/bin/lockx.sh
@@ -10,6 +10,12 @@ blue="$(xgetres color4)FF"
 red="$(xgetres color1)FF"
 green="$(xgetres color10)FF"
 
+# Turn off desk light through Home Assistant
+curl -X POST \
+  -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIwMmZhZGJlODcwYzk0Nzg0OWI2YmE3MDBjOTgxZmJmMyIsImlhdCI6MTYxNzAwNzI3MiwiZXhwIjoxOTMyMzY3MjcyfQ.Obyi-6ZrkMkEOcLZnk5B9VMnmomLJpjWKAAbu0dUwag" \
+  -H "Content-Type: application/json" --data '{"entity_id":"light.scrivania"}' \
+  "http://192.168.1.113:8123/api/services/light/turn_off" &
+
 #scrot --overwrite "$tmpbg"
 #convert "$tmpbg" -scale 50% -scale 10% -scale 1000% -scale 200% "$tmpbg"
 blur_screen.sh $tmpbg
@@ -41,3 +47,4 @@ i3lock --clock --indicator --pass-media-keys \
 --datestr="%a %d %b" \
 \
 -i "$tmpbg"
+

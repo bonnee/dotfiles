cbeaf38c336f1356e8dda833038d9629893a6529
Bonnee <bonora.matteo@gmail.com>
2022-09-07 12:52:53 +0200

    Add santino to screenshot menu

--- .local/bin/screenshot_menu.sh
+++ .local/bin/screenshot_menu.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 base_cmd="grimshot --notify"
+santino_cmd="santino.sh"
 action="$1"
 
-case "$(printf "🗔 Select window\n➕ Select Area\n🖥️ Sceen\n*️ All Screens" | bemenu -p "$action")" in
+case "$(printf "🗔 Select window\n➕ Select Area\n🖥️ Sceen\n😇 Santino\n*️ All Screens" |\
+  bemenu -np "$action")" in
   "🗔 Select window") $base_cmd "$action" window ;;
   "➕ Select Area") $base_cmd "$action" area ;;
   "🖥️ Sceen" )$base_cmd "$action" output ;;
+  "😇 Santino" )$santino_cmd;;
   "*️ All Screens") $base_cmd "$action" screen ;;
 esac
 

cdfaccb26d35c3240bb22d900c2b56a13d807e4a
Bonnee <bonora.matteo@gmail.com>
2024-02-14 09:34:27 +0100

    firefox: add user.js

---
+++ .mozilla/firefox/profile/user.js
@@ -0,0 +1,10 @@
+user_pref("intl.date_time.pattern_override.connector_short", "{1} {0}");
+user_pref("intl.date_time.pattern_override.date_full", "yyyy-MM-dd (EEEEEE)");
+user_pref("intl.date_time.pattern_override.date_long", "yyyy-MM-dd");
+user_pref("intl.date_time.pattern_override.date_medium", "yyyy-MM-dd");
+user_pref("intl.date_time.pattern_override.date_short", "MM-dd");
+user_pref("intl.date_time.pattern_override.time_full", "yyyy-MM-dd HH:mm Z z");
+user_pref("intl.date_time.pattern_override.time_long", "yyyy-MM-dd HH:mm v");
+user_pref("intl.date_time.pattern_override.time_medium", "HH:mm");
+user_pref("intl.date_time.pattern_override.time_short", "HH:mm");
+

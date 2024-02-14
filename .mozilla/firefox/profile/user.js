2b2519e0d0a7028c9be8d8a563ef17eafc60c801
Bonnee <bonora.matteo@gmail.com>
2024-02-14 09:48:16 +0100

    firefox: update user.js

--- .mozilla/firefox/profile/user.js
+++ .mozilla/firefox/profile/user.js
@@ -1,3 +1,4 @@
+// Override locale-provided time format
 user_pref("intl.date_time.pattern_override.connector_short", "{1} {0}");
 user_pref("intl.date_time.pattern_override.date_full", "yyyy-MM-dd (EEEEEE)");
 user_pref("intl.date_time.pattern_override.date_long", "yyyy-MM-dd");
@@ -7,4 +8,8 @@ user_pref("intl.date_time.pattern_override.time_full", "yyyy-MM-dd HH:mm Z z");
 user_pref("intl.date_time.pattern_override.time_long", "yyyy-MM-dd HH:mm v");
 user_pref("intl.date_time.pattern_override.time_medium", "HH:mm");
 user_pref("intl.date_time.pattern_override.time_short", "HH:mm");
+
+// VA-API
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("gfx.webrender.all", true);
 

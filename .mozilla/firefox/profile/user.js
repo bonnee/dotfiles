9fa450e067d0501ba69e909b478f2b96d5c33b09
Bonnee <bonora.matteo@gmail.com>
2024-07-03 15:13:23 +0200

    chore(firefox): add other user_prefs

--- .mozilla/firefox/profile/user.js
+++ .mozilla/firefox/profile/user.js
@@ -9,6 +9,16 @@ user_pref("intl.date_time.pattern_override.time_long", "yyyy-MM-dd HH:mm v");
 user_pref("intl.date_time.pattern_override.time_medium", "HH:mm");
 user_pref("intl.date_time.pattern_override.time_short", "HH:mm");
 
+// Show history before suggestions in URL bar
+user_pref("browser.urlbar.showSearchSuggestionsFirst", false);
+
+// Set compact mode
+user_pref("browser.compactmode.show", true);
+user_pref("browser.uidensity", 1);
+
+// Bookmarks always visible
+user_pref("browser.toolbars.bookmarks.visibility", "always");
+
 // VA-API
 user_pref("media.ffmpeg.vaapi.enabled", true);
 user_pref("gfx.webrender.all", true);
